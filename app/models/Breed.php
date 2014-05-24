0878fbf4bb99bca704bdbae9ca28a810320b5500
James Moreira de Sousa Junior <jamesmsjr@gmail.com>
2014-05-24 19:01:01 -0300

    Cats 1

---
+++ app/models/Breed.php
@@ -0,0 +1,11 @@
+<?php
+
+class Breed extends Eloquent {
+
+    public $timestamps = false;
+
+    public function cats() {
+        return $this->hasMany('Cat');
+    }
+
+}
--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,34 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the Closure to execute when that URI is requested.
+  |
+ */
 
 Route::get('/', 'HomeController@showWelcome');
+
+//Retornar a Página de Gatos.
+Route::get('cats', function() {
+    return "All cats";
+});
+
+//Retornar Gato pelo seu ID.
+Route::get('cats/{id}', function($id) {
+    return "Cat #$id";
+})->where('id', '[0-9]+');
+
+//redirecionar para a página de gatos!
+Route::get('dogs', function() {
+    return Redirect::to('cats');
+});
+
+//Retornar About de Gatos!
+Route::get('cats/about', function() {
+    return View::make('cats/about')->with('number_of_cats', 9000);
+});
